8e9b024b87e8649c65f62d04fea60ced761c3b9e
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-10-25 17:35:27 +0200

    Renaming of class attribute.

--- python_code/image_preprocessing/image_preprocessor.py
+++ python_code/image_preprocessing/image_preprocessor.py
@@ -4,16 +4,16 @@ import tensorflow as tf
 
 
 class ImagePreprocessor:
-    def __init__(self, preprocessing_config=None):
-        """Initializes the ImagePreprocessor with a given configuration.
+    def __init__(self, pipeline=None):
+        """Initializes the ImagePreprocessor with a given pipeline.
 
         Args:
-            preprocessing_config (dict, optional): Configuration dict specifying which preprocessing steps to apply. 
+            pipeline (dict, optional): Pipeline represented as configuartion dict specifying which preprocessing steps to apply. 
                                                    Each key is a step name, and the associated value is a dictionary of arguments for that step.
         """
-        self.config = preprocessing_config or {}
+        self.pipeline = pipeline or {}
 
-    def process(self, images):
+    def process(self, image_dataset):
         """Processes the images according to the provided configuration.
         
         Args:
@@ -22,31 +22,28 @@ class ImagePreprocessor:
         Returns:
             tf.Tensor: Preprocessed images.
         """
-        if "resize" in self.config:
-            images = self.resize_images(images, **self.config["resize"])
-        
-        if "enhance_contrast" in self.config:
-            images = self.enhance_contrast(images, **self.config["enhance_contrast"])
-        
-        if "reduce_noise" in self.config:
-            images = self.reduce_noise(images, **self.config["reduce_noise"])
-        
-        # other steps can be added here similarly
 
-        return images
+        processed_dataset = image_dataset
+        
+        for key in self.pipeline.keys:
+
+            if key == "resize":
+                processed_dataset = self.resize_images(processed_dataset, **self.pipeline["resize"])
+            
+            if key == "enhance_contrast":
+                processed_dataset = self.enhance_contrast(processed_dataset, **self.pipeline["enhance_contrast"])
+            
+            if key == "reduce_noise":
+                processed_dataset = self.reduce_noise(processed_dataset, **self.pipeline["reduce_noise"])
+        
+                # other steps can be added here similarly
+
+        return processed_dataset
 
     def resize_images(self, images, width, height):
         """Resizes the images to the specified dimensions.
-
-        Args:
-            images (tf.Tensor): Input images.
-            width (int): Desired width.
-            height (int): Desired height.
-
-        Returns:
-            tf.Tensor: Resized images.
         """
-        return tf.image.resize(images, [height, width])
+        pass
 
     def enhance_contrast(self, images):
         """Enhances the contrast of the images.
