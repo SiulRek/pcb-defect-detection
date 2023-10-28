82db7368f8d89fa045b5040b1821959c05c068aa
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-10-28 11:47:19 +0200

    Refactoring + allow list of cv2 images.

--- python_code/utils/pcb_visualization.py
+++ python_code/utils/pcb_visualization.py
@@ -1,13 +1,51 @@
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import tensorflow as tf
 
-class PCBVisualizer:
-    """ PCB Visualizer during image processing. Pass slice from tensorflow images as input parameter. """
+class PCBVisualizerBase:
+    """ This class represents the base for the PCBVisualizer child classes."""
 
     def __init__(self):
         self.last_plot = None  
+    
+    def save_plot_to_file(self, filename):
+        if self.last_plot:
+            self.last_plot.savefig(filename)
+        else:
+            print("No plot to save!")
+
+    def _generate_plot(self, fig, title, y_title=0.95, wspace=0.01, hspace=0.01):
+        fig.suptitle(title, fontsize=20, fontweight='bold', y=y_title)
+        plt.subplots_adjust(wspace=wspace, hspace=hspace)
+        plt.show()
+        self.last_plot = plt.gcf()  
+
+    def _get_defect_name(self, target):
+
+        if target == 0:
+            name = 'no_defect'
+        elif target == 1:
+            name = 'missing_hole'
+        elif target == 2:
+            name = 'mouse_bite'
+        elif target == 3:
+            name = 'open_circuit'
+        elif target == 4:
+            name = 'short'
+        elif target == 5:
+            name = 'spur'
+        elif target == 6:
+            name = 'spurious_copper'
+        else:
+            name = 'unknown'
+        
+        return name
+                      
+
+class PCBVisualizerforTF(PCBVisualizerBase):
+    """ PCB Visualizer during image processing. Pass slice from tensorflow images as input parameter. """
 
     def plot_images(self, image_tf_dataset, title="Images"):
         """
@@ -29,12 +67,44 @@ class PCBVisualizer:
                 axes[i].imshow(image_data[0].numpy())
             
             axes[i].axis('off')
+        
+        self._generate_plot(fig, title)
             
-        fig.suptitle(title, fontsize=20, fontweight='bold', y=0.95)
-        plt.subplots_adjust(wspace=0.01, hspace=0.01)
-        plt.show()
+    def plot_image_comparison(self, original_tf_dataset, processed_tf_dataset, index, title=''):
+        """
+        Plots a comparison of original and processed image.
+        
+        Parameters:
+        - original_tf_dataset: TensorFlow dataset containing the origina image.
+        - compare_tf_dataset: TensorFlow dataset containing the compare image.
+        - index: The index number of the images in the corresponding dataset. 
+        - Title: Plot title. Defaults to 'Image Comparison'.
+        """
+        
+        fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+        axes = axes.ravel()
+        
+        image_data_org = original_tf_dataset.skip(index).take(1)
+        image_data_prc = processed_tf_dataset.skip(index).take(1)
 
-        self.last_plot = plt.gcf()  
+        labels = []
+        for i, take_object in enumerate([image_data_org, image_data_prc]):        
+            for image_data in take_object:
+                labels.append(image_data[1])
+                if len(image_data[0].shape) == 3 and image_data[0].shape[2] == 1:
+                    axes[i].imshow(tf.squeeze(image_data[0]).numpy(), cmap='gray')  # use the gray colormap       
+                else:
+                    axes[i].imshow(image_data[0].numpy())
+                
+                axes[i].axis('off')
+
+        if labels[0] != labels[1]:
+            raise ValueError('The compare image probably does not correspond to a processed form of the original image.')
+        
+        if title == '':
+            title = 'Compare Images (' + self._get_defect_name(labels[0]) + ')'
+            
+        self._generate_plot(fig, title)
 
     def plot_histograms(self, image_tf_dataset, title='Histograms', bins=20):
         """
@@ -63,13 +133,16 @@ class PCBVisualizer:
             axes[i].set_xlabel("Color Values", labelpad=10)
             axes[i].set_ylabel("Frequencies", labelpad=10)
 
-        fig.suptitle(title, fontsize=20, fontweight='bold', y=1.03)
-        plt.subplots_adjust(wspace=0.4, hspace=0.4)
-        plt.show()
-
-        self.last_plot = plt.gcf()  
+        self._generate_plot(fig, title,y_title=1.03,wspace=0.4,hspace=0.4)
 
     def plot_frequency_spectrum_3D(self, image_tf_dataset, title="Frequency Spectrum of Images", cmap='viridis'):
+        """    Plots the 3D frequency spectrum of up to 9 images from a TensorFlow dataset.
+
+            Parameters:
+            - image_tf_dataset: TensorFlow dataset containing images.
+            - title: Title for the plot (default is "Frequency Spectrum of Images").
+            - cmap: Colormap used for 3D surface (default is 'viridis').
+        """
 
         fig = plt.figure(figsize=(10, 10))
         fig.suptitle(title, fontsize=16, fontweight='bold', y=1.02)
@@ -113,9 +186,96 @@ class PCBVisualizer:
         magnitude_spectrum = np.abs(f_shift)
 
         return magnitude_spectrum
+    
         
-    def save_plot_to_file(self, filename):
-        if self.last_plot:
-            self.last_plot.savefig(filename)
-        else:
-            print("No plot to save!")
+class PCBVisualizerforCV2(PCBVisualizerBase):
+    """ PCB Visualizer during image processing. Pass list of canvas images as input parameter. """
+
+    def plot_images(self, image_list, title="Images"):
+        """
+        Plots up to 9 images from the given list of images.
+        
+        Parameters:
+        - image_list: List containing the canvas images.
+        - title: Plot title. Defaults to 'Images'.
+        """
+        
+        fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+        axes = axes.ravel()
+        
+        num_images = min(9, len(image_list))
+        
+        for i in range(num_images):
+            
+            # Convert the BGR image from cv2 to RGB for displaying using matplotlib
+            image_data = cv2.cvtColor(image_list[i], cv2.COLOR_BGR2RGB)
+            
+            if image_data.ndim == 3 and image_data.shape[2] == 1:
+                axes[i].imshow(np.squeeze(image_data), cmap='gray')  # use the gray colormap       
+            else:
+                axes[i].imshow(image_data)
+                
+            axes[i].axis('off')
+
+        self._generate_plot(fig, title, y_title=0.95)
+
+    def plot_image_comparison(self, original_img_list, processed_img_list, index, title="Image Comparison"):
+        """
+        Plots a comparison of original and processed image.
+        
+        Parameters:
+        - original_img_list: List containing the original canvas image.
+        - processed_img_list: List containing the processed canvas image.
+        - index: The index number of the images in the corresponding list. 
+        - Title: Plot title. Defaults to 'Image Comparison'.
+        """
+    
+        original_image = cv2.cvtColor(original_img_list[index], cv2.COLOR_BGR2RGB)
+        processed_image = cv2.cvtColor(processed_img_list[index], cv2.COLOR_BGR2RGB)
+        fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+        axes = axes.ravel()
+        
+        images = [original_image, processed_image]
+        
+        for i, image_data in enumerate(images): 
+
+            if len(image_data.shape) == 3 and image_data.shape[2] == 1:
+                axes[i].imshow(np.squeeze(image_data), cmap='gray')  # use the gray colormap
+            else:
+                axes[i].imshow(image_data)
+            
+            axes[i].axis('off')
+        
+        self._generate_plot(fig, title)
+
+   
+    def plot_detected_corners(self, image_list, corners_list, title="Detected Corners"):
+        """
+        Plots up to 9 images with their detected corners from the given list of images.
+        
+        Parameters:
+        - images: List containing the canvas images.
+        - corners_list: List of lists containing the detected corners for each image.
+        - title: Plot title. Defaults to 'Detected Corners'.
+        """
+        
+        fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+        axes = axes.ravel()
+        
+        for i in range(9):
+            
+            image_data = cv2.cvtColor(image_list[i], cv2.COLOR_BGR2RGB)
+            axes[i].imshow(image_data)
+            
+            # Plot the detected corners on the image
+
+            for j, corner in enumerate(corners_list[i]):
+                axes[i].scatter(corner[1], corner[0], s=100, c='red', marker='o')
+                axes[i].text(corner[1], corner[0], str(j), color='orange')
+                
+            axes[i].set_title(f"Detected Corners: {len(corners_list[i])}", color='red')
+            
+            axes[i].axis('off')
+            
+        self._generate_plot(fig, title, y_title=0.95)
+
