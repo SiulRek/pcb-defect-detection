c4e52b8fce8552c4566b2ed7760aaba78663daab
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-10-31 09:35:30 +0100

    Add first working step.

--- python_code/image_preprocessing/image_preprocessing_steps.py
+++ python_code/image_preprocessing/image_preprocessing_steps.py
@@ -2,11 +2,46 @@ import tensorflow as tf
 import cv2
 
 class StepBase:
+    """   Base class for defining preprocessing steps for images.
+
+    Attributes:
+    - Name (str)
+        A name identifier for the preprocessing step.
+    - params (dict)
+        A dictionary containing parameters needed for the preprocessing step.
+
+    Methods:
+    - process_step(tf_image: tf.Tensor, tf_target: tf.Tensor) -> (tf.Tensor, tf.Tensor):
+        Child class must implement this method for preprocessing functionality.
+
+    - extract_params(local_vars: dict) -> dict:
+        Extracts parameters needed for the preprocessing step.
+
+    - tf_function_decorator(func: Callable) -> Callable:
+        Decorator to wrap TensorFlow functions for mapping onto a dataset.
+
+    - py_function_decorator(func: Callable) -> Callable:
+        Decorator to wrap python functions for mapping onto a dataset using tf.py_function.
+
+    - random_params() -> None:
+        Child class can implement this method to randomize parameters.
+
+    - _reshape_color_channel(tf_image: tf.Tensor, color_channel: str = 'gray', tf_image_comparison: tf.Tensor = None) -> tf.Tensor:
+        Helper method to reshape image tensor based on color channel.
+
+    Notes:
+    - The class is designed to be subclassed for specific preprocessing steps.
+    - Each child class must implement the `process_step` method.
+    """
 
     def __init__(self, name,  local_vars):
         self.name = name
         self.params = self.extract_params(local_vars)
         
+    def process_step(self, tf_image, tf_target):
+        # Child class must implement this method.
+        pass
+    
     def extract_params(self, local_vars):
 
         if local_vars['set_random_params']:
@@ -16,27 +51,30 @@ class StepBase:
     @staticmethod
     def tf_function_decorator(func):
         def wrapper(self, image_dataset):
-            return image_dataset.map(func)
+            def mapped_function(img, tgt):
+                return func(self, img, tgt)
+            return image_dataset.map(mapped_function)
         return wrapper
 
     @staticmethod
     def py_function_decorator(func):
-        def wrapper(image_dataset):
-            return image_dataset.map(
-                lambda img, tgt: tf.py_function(func=func, inp=[img, tgt], Tout=(tf.uint8, tf.int8))
-            )
+        def wrapper(self, image_dataset):
+            def mapped_function(img, tgt):
+                processed_img, processed_tgt = tf.py_function(
+                    func=lambda image, target: func(self, image, target),  # Lambda is used to pass self.
+                    inp=[img, tgt],
+                    Tout=(tf.uint8, tf.int8),
+                )
+                return processed_img, processed_tgt
+            return image_dataset.map(mapped_function)
         return wrapper
-    
+
     def random_params(self): 
         # Child class must implement this method.
         pass 
 
-    def process_step(self, tf_image, tf_target):
-        # Child class must implement this method.
-        pass
-
     def _reshape_color_channel(self, tf_image, color_channel='gray', tf_image_comparison=None):
-        if tf_image_comparison:
+        if tf_image_comparison is not None:
             return tf.reshape(tf_image, [tf_image.shape[0], tf_image.shape[1], tf_image_comparison.shape[2]])
         elif color_channel == 'gray':
             return tf.reshape(tf_image, [tf_image.shape[0], tf_image.shape[1], 1])
--- python_code/image_preprocessing/image_preprocessor.py
+++ python_code/image_preprocessing/image_preprocessor.py
@@ -4,11 +4,27 @@ from copy import deepcopy
 from python_code.image_preprocessing.image_preprocessing_steps import StepBase
 
 class ImagePreprocessor:
+    """ A class to define and process the PCB image preprocessing pipeline.
+
+    Attributes:
+    - pipeline (list)
+        List containing the steps (which are children of StepBase) to be executed in the preprocessing pipeline.
+
+    Methods:
+    - add_step(step: StepBase) -> None:
+        Adds a preprocessing step to the current pipeline.
+
+    - process(image_dataset: tf.data.Dataset) -> tf.data.Dataset:
+        Processes the provided image dataset through the defined preprocessing pipeline.
+
+    Notes:
+    - The pipeline is executed in the order the steps are added.
+    - Each step in the pipeline should be an instance of a child class of StepBase.
+
+    """
     def __init__(self, pipeline=None): 
-        
         if pipeline is None:
             pipeline = []
-
         self._pipeline = deepcopy(pipeline)
 
     @property
@@ -17,11 +33,9 @@ class ImagePreprocessor:
 
     @pipeline.setter
     def pipeline(self, pipeline):
-
         for step in pipeline:
             if not isinstance(step, StepBase):  
-                raise ValueError(f'Expecting a Child of StepBase, got {type(step)} instead.')
-            
+                raise ValueError(f'Expecting a Child of StepBase, got {type(step)} instead.')        
         self._pipeline = deepcopy(pipeline)
 
     def add_step(self, step):
@@ -31,6 +45,7 @@ class ImagePreprocessor:
 
     def process(self, image_dataset):
 
+        #TODO: Add Error handling here.
         processed_dataset = image_dataset
         for step in self.pipeline:
             processed_dataset = step.process_step(processed_dataset)
