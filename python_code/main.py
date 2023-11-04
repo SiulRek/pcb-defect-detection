c987de24043e16445793ffadb9f66dc18d636565
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-11-04 18:33:59 +0100

    Minor changes in template.

--- python_code/main.py
+++ python_code/main.py
@@ -1,21 +1,25 @@
 from python_code.image_preprocessing.image_preprocessor import ImagePreprocessor
 from python_code.model.pcb_defect_detector import PCBDefectDetector
 
-#IMPORTANT ANNOTATIONS: The current functionality serves just as a template and as documentation of the current software plan.
+#IMPORTANT ANNOTATIONS: The current functionality serves just as a template and as documentation of the current software plan.  Simplified Training procedure is shown here.
 
 
 if __name__ == '__main__':
-
-    preprocessing_config = {
-        'resize': {'width': 2000, 'height': 2000},
-        'enhance_contrast': {'factor': 1.5},
-        'reduce_noise': {'method': 'gaussian'}
+    pipeline = {
+        # Add preprocessing steps here.
+    }
+    
+    model = {
+        # Add model layers here.
     }
 
     train_data, test_data = (None, None)
 
-    preprocessor = ImagePreprocessor(preprocessing_config)
-    detector = PCBDefectDetector(preprocessor)
-    detector.build_model()
-    history = detector.train(train_data)
-    detector.predict(test_data)
+    preprocessor = ImagePreprocessor()
+    preprocessor.set_pipe(pipeline)
+    processed_train_data = preprocessor.process(train_data)
+    processed_test_data = preprocessor.process(test_data)
+    detector = PCBDefectDetector()
+    detector.build_model(model)
+    history = detector.train(processed_train_data)
+    detector.predict(processed_test_data)
