399c18157253618e2a357a15f49988b0c7b685d6
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-11-17 14:30:03 +0100

    add tests to check if all implented distributions work.

--- python_code/utils/tests/get_sample_from_distribution_test.py
+++ python_code/utils/tests/get_sample_from_distribution_test.py
@@ -7,12 +7,44 @@ class TestGetSampleFromDistribution(unittest.TestCase):
         data = {'distribution': 'gaussian', 'loc': 0, 'scale': 1}
         sample = get_sample_from_distribution(data)
         self.assertIsInstance(sample, float)
+        
+    def test_gaussian(self):
+        data = {'distribution': 'gaussian', 'loc': 0, 'scale': 1}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
 
-    def test_uniform_distribution(self):
+    def test_uniform(self):
         data = {'distribution': 'uniform', 'low': 1, 'high': 2}
         sample = get_sample_from_distribution(data)
         self.assertIsInstance(sample, float)
         self.assertTrue(1 <= sample <= 2)
+
+    def test_exponential(self):
+        data = {'distribution': 'exponential', 'scale': 1}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_poisson(self):
+        data = {'distribution': 'poisson', 'lam': 3}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_binomial(self):
+        data = {'distribution': 'binomial', 'n': 10, 'p': 0.5}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_gamma(self):
+        data = {'distribution': 'gamma', 'shape': 2, 'scale': 1}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_beta(self):
+        data = {'distribution': 'beta', 'a': 0.5, 'b': 0.5}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_lognormal(self):
+        data = {'distribution': 'lognormal', 'mean': 0, 'sigma': 1}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
+
+    def test_laplace(self):
+        data = {'distribution': 'laplace', 'loc': 0, 'scale': 1}
+        self.assertIsInstance(get_sample_from_distribution(data), float)
 
     def test_invalid_distribution(self):
         data = {'distribution': 'unknown', 'param': 1}
