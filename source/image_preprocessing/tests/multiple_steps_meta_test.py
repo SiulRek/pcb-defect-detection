538acbc0d80c8713d1c0662691312fa366c37284
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-11-28 10:13:58 +0100

    Update after changes of the tested framework.

--- source/image_preprocessing/tests/multiple_steps_meta_test.py
+++ source/image_preprocessing/tests/multiple_steps_meta_test.py
@@ -14,46 +14,43 @@ class TestTestFramework(unittest.TestCase):
         TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)})
         self.assertEqual(TestClass.__name__, 'TestAdaptiveHistogramEqualizer')
         self.assertTrue(issubclass(TestClass, TestSingleStep))
-        self.assertTrue(hasattr(TestClass, 'StepClass'))
+        self.assertTrue(hasattr(TestClass, 'TestStep'))
         self.assertTrue(hasattr(TestClass, 'params'))
         test_instance = TestClass('test_load_from_json')  # 'test_load_from_json' can be any existing test method name
-        self.assertEqual(test_instance.StepClass, ExampleStep)
+        self.assertEqual(test_instance.TestStep, ExampleStep)
         self.assertEqual(test_instance.params,{'clip_limit': 1.0, 'tile_gridsize': (5, 5)})
 
 
 class TestConditionalSkipping(unittest.TestCase):
 
-    def test_visual_inspection_skipping(self):
+    def test_visual_inspection_skipping_1(self):
         with patch('source.image_preprocessing.tests.multiple_steps_test.ENABLE_VISUAL_INSPECTION', False):
-            TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)}, grayscale_only=True)
+            TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)})
         suite = unittest.TestLoader().loadTestsFromTestCase(TestClass)
         result = unittest.TextTestRunner().run(suite)
         skipped_test_names = [test[0].id().split('.')[-1] for test in result.skipped]
         self.assertIn('test_processed_image_visualization', skipped_test_names)
         TestClass = create_test_class_for_step(ExampleStep, {})
 
-    def test_grayscale_only_skipping(self):
-        TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)}, grayscale_only=True)
+    def test_visual_inspection_skipping_2(self):
+        with patch('source.image_preprocessing.tests.multiple_steps_test.ENABLE_VISUAL_INSPECTION', True):
+            TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)}, visual_inspection_always_disable=True)
         suite = unittest.TestLoader().loadTestsFromTestCase(TestClass)
         result = unittest.TextTestRunner().run(suite)
         skipped_test_names = [test[0].id().split('.')[-1] for test in result.skipped]
-        self.assertIn('test_process_rgb_images', skipped_test_names)
+        self.assertIn('test_processed_image_visualization', skipped_test_names)
+        TestClass = create_test_class_for_step(ExampleStep, {})
 
     def test_visual_inspection_not_skipping(self):
         with patch('source.image_preprocessing.tests.multiple_steps_test.ENABLE_VISUAL_INSPECTION', True):
-            TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)}, grayscale_only=True)
+            TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)})
         suite = unittest.TestLoader().loadTestsFromTestCase(TestClass)
         result = unittest.TextTestRunner().run(suite)
         skipped_test_names = [test[0].id().split('.')[-1] for test in result.skipped]
         self.assertNotIn('test_processed_image_visualization', skipped_test_names)
         TestClass = create_test_class_for_step(ExampleStep, {})
 
-    def test_grayscale_only_not_skipping(self):
-        TestClass = create_test_class_for_step(ExampleStep, {'clip_limit': 1.0, 'tile_gridsize': (5, 5)}, grayscale_only=False)
-        suite = unittest.TestLoader().loadTestsFromTestCase(TestClass)
-        result = unittest.TextTestRunner().run(suite)
-        skipped_test_names = [test[0].id().split('.')[-1] for test in result.skipped]
-        self.assertNotIn('test_process_rgb_images', skipped_test_names)
+
 
 if __name__ == '__main__':
     unittest.main()
