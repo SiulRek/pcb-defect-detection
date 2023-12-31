d66f11d80e1cf53c8105611608428b3f3bd10ab5
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-12-31 11:07:57 +0100

    Refactoring to improve readability.

--- source/utils/randomly_select_sequential_keys.py
+++ source/utils/randomly_select_sequential_keys.py
@@ -1,21 +1,25 @@
 import random
 import re
 
-import random
-import re
-
 
 def is_sequential(lst):
-    """
-    Check if the list elements start from 0 and increment by 1 with each element.
-
-    Args:
-    - lst (list): List to check.
-    
-    Returns:    
-    - (bool): Boolean indicating whether the list is sequential starting from 0.
-    """
+    """Check if the list elements start from 0 and increment by 1 with each element."""
     return all(i == val for i, val in enumerate(lst))
+
+
+def matches_pattern(key, pattern):
+    """Check if a key matches a given regular expression pattern."""
+    return pattern.match(key) is not None
+
+
+def extract_indices(input_dict, pattern):
+    """Extract indices from keys in the input dictionary based on a given pattern."""
+    return [int(pattern.match(key).group(1)) for key in input_dict if matches_pattern(key, pattern)]
+
+
+def filter_keys_by_pattern(input_dict, pattern):
+    """Filter keys in the input dictionary that match a given pattern."""
+    return [key for key in input_dict if matches_pattern(key, pattern)]
 
 
 def randomly_select_sequential_keys(input_dict, separator='__'):
@@ -29,34 +33,30 @@ def randomly_select_sequential_keys(input_dict, separator='__'):
     Returns:
     - (dict): A new dictionary containing randomly selected keys and their corresponding values.
     """
+    seq_key_pattern = re.compile(rf'.*{re.escape(separator)}L(\d+)')
+    seq_key_pattern_end = re.compile(rf'.*{re.escape(separator)}L(\d+)$')
+    freq_key_pattern = re.compile(rf'.*{re.escape(separator)}L\d+F(\d+)$')
 
-    seq_key_pattern = re.compile(r'.*' + re.escape(separator) + r'L(\d+)')
-    seq_key_pattern_end = re.compile(r'.*' + re.escape(separator) + r'L(\d+)$')
-    freq_key_pattern = re.compile(r'.*' + re.escape(separator) + r'L\d+F(\d+)$')   
-            
-    match_flags = [
-        True if seq_key_pattern_end.match(key) or freq_key_pattern.match(key) else False
-        for key in input_dict
-    ]
-
+    match_flags = [matches_pattern(key, seq_key_pattern_end) or matches_pattern(key, freq_key_pattern) for key in input_dict]
     if not any(match_flags):
         return input_dict
-    elif not all(match_flags):
-        raise KeyError('Some keys do not follow a sequential pattern. Please ensure that all keys follow a sequential pattern.')
-    
-    indices = list(set([seq_key_pattern.match(key).group(1) for key in input_dict]))
-    indices.sort()
-    if not is_sequential([int(i) for i in indices]):
-        raise KeyError('The indices of the keys are not sequential. Please ensure that the keys follow a sequential pattern.')
-    
+
+    if not all(match_flags):
+        raise KeyError('Some keys do not follow a sequential pattern.')
+
+    indices = extract_indices(input_dict, seq_key_pattern)
+    unique_sorted_indices = sorted(set(indices))
+    if not is_sequential(unique_sorted_indices):
+        raise KeyError('Indices of the keys are not sequential.')
+
     output_dict = {}
-    for index in indices:
-        temp_keys = [key for key in input_dict if seq_key_pattern.match(key).group(1) == index]
+    for index in unique_sorted_indices:
+        temp_keys = filter_keys_by_pattern(input_dict, re.compile(rf'.*{re.escape(separator)}L{index}(F\d+|$)'))
         keys = []
         for key in temp_keys:
-            if freq_key_pattern.match(key):
-                freq = int(freq_key_pattern.match(key).group(1))
-                keys.extend([key]*freq)
+            if match := freq_key_pattern.match(key):
+                freq = int(match.group(1))
+                keys.extend([key] * freq)
             else:
                 keys.append(key)
 
