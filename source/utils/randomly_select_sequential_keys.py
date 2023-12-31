2e2111729e485801f50917be7046ae5c15a302ef
SiulRek <luis.kraker@edu.fh-joanneum.at>
2023-12-31 17:07:22 +0100

    Allow string to continue after sequence pattern + change index specification to 'I'.

--- source/utils/randomly_select_sequential_keys.py
+++ source/utils/randomly_select_sequential_keys.py
@@ -38,44 +38,54 @@ def remove_second_group(s, pattern):
     if not match:
         return s
 
-    def replace_with_first_group(match):
-        return match.group(1)
+    def replace_with_first_and_third_group(match):
+        return match.group(1) + match.group(3) 
 
-    return pattern.sub(replace_with_first_group, s)
+    return pattern.sub(replace_with_first_and_third_group, s)
 
 
 def randomly_select_sequential_keys(input_dict, separator='__'):
     """
-    Randomly selects keys from a dictionary that follow a sequential pattern.
+    Randomly selects keys from a dictionary that follow a specific sequential pattern. The pattern is defined by a separator
+      followed by 'I' and a number, optionally followed by 'F' and another number. The function selects keys based on their 
+      indices and frequencies, if specified.
 
     Args:
     - input_dict (dict): The input dictionary containing keys to be selected.
-    - separator (str, optional): The separator used in the key pattern. Defaults to '__'.
+    - separator (str, optional): The separator used in the key pattern, defaulting to '__'.
 
     Returns:
     - (dict): A new dictionary containing randomly selected keys and their corresponding values.
-    """
-    seq_key_pattern = re.compile(rf'(.*?){re.escape(separator)}L(\d+)')
-    seq_key_pattern_full = re.compile(rf'(.*?)({re.escape(separator)}L\d+)')
-    seq_key_pattern_end = re.compile(rf'(.*?){re.escape(separator)}L(\d+)$')
-    freq_key_pattern = re.compile(rf'(.*?){re.escape(separator)}L\d+F(\d+)$')
-    freq_key_pattern_full = re.compile(rf'(.*?)({re.escape(separator)}L\d+F\d+)$')
 
-    match_flags = [matches_pattern(key, seq_key_pattern_end) or matches_pattern(key, freq_key_pattern) for key in input_dict]
+    The function first checks if each key matches either the index pattern (e.g., '__I1') or the frequency pattern 
+    (e.g., '__I1F2'). If all keys match one of these patterns, the function then extracts the indices from the keys 
+    and checks if they form a sequential series starting from 0. Afterward, it randomly selects one key per index, 
+    considering the frequency of keys where specified. The selected keys are then processed to remove the pattern, preserving 
+    the parts of the key without the specific sequential pattern.
+    """
+
+    end_pattern = rf'($|{re.escape(separator)}\S+)'
+    ind_key_pattern = re.compile(rf'(.*?){re.escape(separator)}I(\d+)(.*?)')
+    ind_key_pattern_all_groups = re.compile(rf'(.*?)({re.escape(separator)}I\d+)(.*?)')
+    ind_key_pattern_end = re.compile(rf'(.*?){re.escape(separator)}I(\d+){end_pattern}')
+    freq_key_pattern = re.compile(rf'(.*?){re.escape(separator)}I\d+F(\d+){end_pattern}')
+    freq_key_pattern_all_groups = re.compile(rf'(.*?)({re.escape(separator)}I\d+F\d+){end_pattern}')
+
+    match_flags = [matches_pattern(key, ind_key_pattern_end) or matches_pattern(key, freq_key_pattern) for key in input_dict]
     if not any(match_flags):
         return input_dict
 
     if not all(match_flags):
         raise KeyError('Some keys do not follow a sequential pattern.')
 
-    indices = extract_indices(input_dict, seq_key_pattern)
+    indices = extract_indices(input_dict, ind_key_pattern)
     unique_sorted_indices = sorted(set(indices))
     if not is_sequential(unique_sorted_indices):
         raise KeyError('Indices of the keys are not sequential.')
 
     output_dict = {}
     for index in unique_sorted_indices:
-        temp_keys = filter_keys_by_pattern(input_dict, re.compile(rf'.*{re.escape(separator)}L{index}(F\d+|$)'))
+        temp_keys = filter_keys_by_pattern(input_dict, re.compile(rf'.*{re.escape(separator)}I{index}(F\d+|{end_pattern})'))
         keys = []
         for key in temp_keys:
             if match := freq_key_pattern.match(key):
@@ -85,8 +95,8 @@ def randomly_select_sequential_keys(input_dict, separator='__'):
                 keys.append(key)
 
         selected_key = random.choice(keys)
-        new_key = remove_second_group(selected_key, freq_key_pattern_full)
-        new_key = remove_second_group(new_key, seq_key_pattern_full)
+        new_key = remove_second_group(selected_key, freq_key_pattern_all_groups)
+        new_key = remove_second_group(new_key, ind_key_pattern_all_groups)
         output_dict[new_key] = input_dict[selected_key]
 
     return output_dict
