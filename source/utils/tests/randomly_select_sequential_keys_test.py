33ae29b4e7acb460457fee6854fbee0109e8fed0
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-01-02 10:50:27 +0100

    Refactoring of test cases to improve readability.

--- source/utils/tests/randomly_select_sequential_keys_test.py
+++ source/utils/tests/randomly_select_sequential_keys_test.py
@@ -10,7 +10,6 @@ ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..','
 OUTPUT_DIR = os.path.join(ROOT_DIR, r'source/utils/tests/outputs')
 LOG_FILE = os.path.join(OUTPUT_DIR, 'test_results.log')
 
-#TODO: Make Test Suite more readable
 
 class TestRandomlySelectSequentialKeys(unittest.TestCase):
     """
@@ -22,7 +21,14 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
     and error handling, verifying its consistency across different key configurations and
     input scenarios.
 
-    Note: All test cases take the default separator value of '__' for simplicity.
+    Note: 
+    - All test cases take the default separator value of '__' for simplicity.
+    - The naming convention for the dictionary key looks like this:
+        {key identifier letter}_{key}_i{index}__I{index}F{frequency}__extra
+        ° Only the 'key' is required, the rest are dependent on the test cases.
+        ° __I{index}F{frequency} is the pattern that the function looks for.
+        ° i{index} is used to verify the operation as the pattern __I{index}F{frequency}
+        will be removed in the output dictionary.
     """
 
 
@@ -51,7 +57,7 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """ 
         Test that the function raises an error when only some keys do not match the pattern.
         """
-        input_dict = {"name__I0": "value1", "B2": "value2"}
+        input_dict = {"a_key__I0": "value1", "b_key": "value2"}
         with self.assertRaises(KeyError):
             randomly_select_sequential_keys(input_dict)
 
@@ -59,7 +65,7 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """ 
         Test that the function raises an error when the indices are not sequential.
         """
-        input_dict = {"name1__I1": "value1", "name2__I3": "value2"}
+        input_dict = {"a_key_i1__I1": "value1", "b_key_i3__I3": "value2"}
         with self.assertRaises(KeyError):
             randomly_select_sequential_keys(input_dict)
 
@@ -67,23 +73,23 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """ 
         Test that all keys are selected when all keys match the pattern and have different indices.
         """
-        input_dict = {"name0__I0": "value0", "name1__I1": "value1", "name2__I2": "value2"}
+        input_dict = {"a_key_i0__I0": "value0", "b_key_i1__I1": "value1", "c_key_i2__I2": "value2"}
         output_dict = randomly_select_sequential_keys(input_dict)
         stripped_input_keys = self.get_stripped_dict_keys(input_dict)
         self.assertTrue(all(key in stripped_input_keys for key in output_dict))
         self.assertEqual(len(output_dict), 3)
-        self.assertTrue(is_sequential([int(key.split('name')[1]) for key in output_dict]))
+        self.assertTrue(is_sequential([int(key.split('i')[1]) for key in output_dict]))
 
     def test_normal_operation(self):
         """ 
         Test the normal operation of the function.
         """
-        input_dict = {"a_name0__I0": "value0", "b_name0__I0": "alt0", "a_name1__I1": "value1", "b_name1__I1": "alt1"}
+        input_dict = {"a_key_i0__I0": "value0", "b_key_i0__I0": "alt0", "a_key_i1__I1": "value1", "b_key_i1__I1": "alt1"}
         output_dict = randomly_select_sequential_keys(input_dict)
         stripped_input_keys = self.get_stripped_dict_keys(input_dict)
         self.assertTrue(all(key in stripped_input_keys for key in output_dict))
         self.assertEqual(len(output_dict), 2)
-        self.assertTrue(is_sequential([int(key.split('name')[1]) for key in output_dict]))
+        self.assertTrue(is_sequential([int(key.split('i')[1]) for key in output_dict]))
     
     def _generate_test_data(self, num_sequences):
         """
@@ -95,7 +101,7 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         Returns:
         - (dict): A dictionary with generated test data.
         """
-        return {f"{i % 2}_name{i // 2}__I{i // 2}": f"value{i}" for i in range(num_sequences * 2)}
+        return {f"{i % 2}_key_i{i // 2}__I{i // 2}": f"value{i}" for i in range(num_sequences * 2)}
 
     def test_normal_operation_with_long_sequence(self):
         """ 
@@ -107,40 +113,40 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         stripped_input_keys = self.get_stripped_dict_keys(input_dict)
         self.assertTrue(all(key in stripped_input_keys for key in output_dict))
         self.assertEqual(len(output_dict), num_sequences)
-        self.assertTrue(is_sequential([int(key.split('name')[1]) for key in output_dict]))
+        self.assertTrue(is_sequential([int(key.split('i')[1]) for key in output_dict]))
 
     def test_resilient_operation_1(self):
         """ 
         Test that the function is resilient to unique identifier specified in keys.
         """
-        input_dict = {"name1__1__I1": "value1", "name1__2__I1": "alt1", "name0__3__I0": "value0",  "name0__4__I0": "alt0"}
+        input_dict = {"key_i1__1__I1": "value1", "key_i1__2__I1": "alt1", "key_i0__3__I0": "value0",  "key_i0__4__I0": "alt0"}
         output_dict = randomly_select_sequential_keys(input_dict)
-        stripped_input_keys = ['name1__1', 'name1__2', 'name0__3', 'name0__4']
+        stripped_input_keys = ['key_i1__1', 'key_i1__2', 'key_i0__3', 'key_i0__4']
         self.assertTrue(all(key in stripped_input_keys for key in output_dict))
         self.assertEqual(len(output_dict), 2)
-        self.assertTrue(is_sequential([int(key.split('name')[1][0]) for key in output_dict]))
+        self.assertTrue(is_sequential([int(key.split('i')[1][0]) for key in output_dict]))
     
     def test_resilient_operation_2(self):
         """ 
         Test that the function is resilient to the order of the keys.
         """
-        input_dict = {"aname1__I1": "value1", "aname0__I0": "value0", "bname1__I1": "alt1", "bname0__I0": "alt0"}
+        input_dict = {"a_key_i1__I1": "value1", "b_key_i0__I0": "value0", "c_key_i1__I1": "alt1", "d_key_i0__I0": "alt0"}
         output_dict = randomly_select_sequential_keys(input_dict)
         stripped_input_keys = self.get_stripped_dict_keys(input_dict)
         self.assertTrue(all(key in stripped_input_keys for key in output_dict))
         self.assertEqual(len(output_dict), 2)
-        self.assertTrue(is_sequential([int(key.split('name')[1]) for key in output_dict]))
+        self.assertTrue(is_sequential([int(key.split('i')[1]) for key in output_dict]))
 
     def test_keys_with_frequency_simple(self):
         """ 
         Test that keys with frequency specification are processed correctly.
         """
         input_dict = {
-            "name1__I0": "value0", 
-            "name2__I0F10": "alt0", 
-            "name3__I1": "value1", 
-            "name4__I1F10": "alt1",
-            "name5__I2F10": "alt2",
+            "a_key__I0": "value0", 
+            "b_key__I0F10": "alt0", 
+            "c_key__I1": "value1", 
+            "d_key__I1F10": "alt1",
+            "e_key__I2F10": "alt2",
         }
         output_dict = randomly_select_sequential_keys(input_dict)
         stripped_input_keys = self.get_stripped_dict_keys(input_dict)
@@ -153,12 +159,12 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """
 
         input_dict = {
-            "name1__I0": "value0", 
-            "name2__I0F10": "alt0", 
-            "name3__I1": "value1", 
-            "name4__I1F10": "alt1"
+            "a_key__I0": "value0", 
+            "b_key__I0F10": "alt0", 
+            "c_key__I1": "value1", 
+            "d_key__I1F10": "alt1"
         }
-        keys = ['name1', 'name2', 'name3', 'name4']
+        keys = ['a_key', 'b_key', 'c_key', 'd_key']
         output_dicts = []
         for _ in range(1000):
             output_dicts.append(randomly_select_sequential_keys(input_dict))
@@ -167,8 +173,8 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         for key in keys:
             key_counts[key] = sum([1 if key in output_dict else 0 for output_dict in output_dicts])
 
-        self.assertAlmostEqual(key_counts["name1"], 100, delta=40)
-        self.assertAlmostEqual(key_counts["name3"], 100, delta=40)  
+        self.assertAlmostEqual(key_counts["a_key"], 100, delta=40)
+        self.assertAlmostEqual(key_counts["c_key"], 100, delta=40)  
    
     def test_pattern_ending_allowed(self):
         """
@@ -176,19 +182,19 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """
         separator = '__'
         input_dict = {
-            f"name1{separator}extra{separator}I0": "value0", 
-            f"name2{separator}I1": "value1", 
-            f"name3{separator}I2{separator}extra": "value2",  
-            f"name4{separator}I3F10": "value3",  
-            f"name5{separator}I4F10{separator}extra": "value4" 
+            f"a_key{separator}extra{separator}I0": "value0", 
+            f"b_key{separator}I1": "value1", 
+            f"c_key{separator}I2{separator}extra": "value2",  
+            f"d_key{separator}I3F10": "value3",  
+            f"e_key{separator}I4F10{separator}extra": "value4" 
         }
 
         expected_dict = {
-            f"name1{separator}extra": "value0",
-            f"name2": "value1",
-            f"name3{separator}extra": "value2",
-            f"name4": "value3",
-            f"name5{separator}extra": "value4"
+            f"a_key{separator}extra": "value0",
+            f"b_key": "value1",
+            f"c_key{separator}extra": "value2",
+            f"d_key": "value3",
+            f"e_key{separator}extra": "value4"
         }
       
         output_dict = randomly_select_sequential_keys(input_dict)
@@ -200,11 +206,11 @@ class TestRandomlySelectSequentialKeys(unittest.TestCase):
         """
         separator = '__'
         input_dict = {
-            f"name1{separator}I0": "value0", 
-            f"name2{separator}I1": "value1", 
-            f"name3{separator}I2_extra": "value2",  
-            f"name4{separator}I3F10": "value3",  
-            f"name5{separator}I4F10_extra": "value4" 
+            f"a_key{separator}I0": "value0", 
+            f"b_key{separator}I1": "value1", 
+            f"c_key{separator}I2_extra": "value2",  
+            f"d_key{separator}I3F10": "value3",  
+            f"e_key{separator}I4F10_extra": "value4" 
         }
 
         with self.assertRaises(KeyError):
