9e666d1cf50bdb1b70ee48e97eb74435bc4683a3
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-03-19 11:25:18 +0100

    Refactor StepBase: Separate target, simplify output datatype.

--- source/image_preprocessing/preprocessing_steps/step_base.py
+++ source/image_preprocessing/preprocessing_steps/step_base.py
@@ -1,43 +1,40 @@
 from abc import ABC, abstractmethod
-from copy import deepcopy
 import tensorflow as tf
-
 
 from source.image_preprocessing.preprocessing_steps.step_utils import correct_image_tensor_shape
 
-class StepBase(ABC):
-    """  Base class for defining preprocessing steps for images in a image preprocessing pipeline.
 
-    This abstract class provides a structured approach to implementing various image preprocessing steps. 
-    Each step is characterized by its unique parameters and functionality, which are defined in the child classes 
-    inheriting from StepBase. The class facilitates the integration and execution of preprocessing steps within a 
-    TensorFlow image processing pipeline.
+class StepBase(ABC):
+    """
+    Base class for defining preprocessing steps for images in an image preprocessing pipeline.
+
+    This abstract class provides a structured approach to implementing various image preprocessing steps.
+    Each step is characterized by its unique parameters and functionality, which are defined in the child
+    classes inheriting from StepBase. The class facilitates the integration and execution of preprocessing
+    steps within a TensorFlow image processing pipeline.
 
     Child classes should implement the `process_step` method according to their specific processing requirements and
     specify the value of the class attributes: `arguments_datatype` and `name`.
-    Decorators `_tensor_pyfunc_wrapper` and `_nparray_pyfunc_wrapper` are provided for flexibility in implementing 
+
+    Decorators `_tensor_pyfunc_wrapper` and `_nparray_pyfunc_wrapper` are provided for flexibility in implementing
     TensorFlow and Python functions, respectively.
 
-    Public Class Attribute (read-ony):
-    - defaults_output_datatypes (dict):  A dictionary containing the defaults values of the argument datatypes.
-    - arguments_datatype (dict):  A dictionary containing the preprocessing step
-    specific values of the argument datatypes. Defaults to defaults_output_datatypes.
+    Public Class Attribute (read-only):
+    - default_output_datatype (dtype): Default datatype for the output image tensor, can be overridden in child classes.
+    - arguments_datatype (dtype, optional): Datatype for the preprocessing step's arguments. If not defined, defaults to `default_output_datatype`.
     - name (str): The base identifier for the preprocessing step.
 
-    Public Instance Attribute (read-ony):
-    - parameters (dict):  A dictionary containing parameters needed for the preprocessing step.
+    Public Instance Attribute (read-only):
+    - parameters (dict): A dictionary containing parameters needed for the preprocessing step.
 
     Public Methods:
-    - process_step(image: tf.Tensor or np.array) -> tf.Tensor or np.array:
-        To be implemented by the child class to define the specific preprocessing functionality. The image datatype depends
-        on the function_decorator used, either `_nparray_pyfunc_wrapper`  for processing tensors  or `_tensor_pyfunc_wrapper`
-        for processing np.arrays.
-
+    - process_step(image_tensor: tf.Tensor, tf_target: Any) -> tf.Tensor:
+        To be implemented by the child class to define the specific preprocessing functionality. The method
+        takes an image tensor and an optional target, returning the processed image tensor.
 
     Child Class Template:
         class StepTemplate(StepBase):
-
-            arguments_datatype = <dictionary specifying the argument datatypes>
+            arguments_datatype = <datatype for arguments>
             name = <Preprocessing step identifier>
             def __init__(self, **processing_step_specific_args):
                 super().__init__(locals())
@@ -49,13 +46,13 @@ class StepBase(ABC):
                 return image_tensor_processed
 
     TODOs when integrating a new preprocessing step in the framework:
-        1. Create preprocessing step class inheriting from `StepBase` according to template.
-        2. Add mapping of the class to the constant `STEP_CLASS_MAPPING` {<self._name>:type(self)}.
+        1. Create preprocessing step class inheriting from `StepBase` according to the template.
+        2. Add mapping of the class to the constant `STEP_CLASS_MAPPING` {<self.name>: type(self)}.
         3. Add JSON entry of the class to .source/image_preprocessing/pipeline/template.json
         4. Execute single_step_test.py over this class.
     """
     
-    default_output_datatypes = {'image': tf.uint8, 'target': tf.int8} 
+    default_output_datatype = tf.uint8
     # Child Classes have to overwrite this attributes
     arguments_datatype = None   
     name = None
@@ -73,7 +70,7 @@ class StepBase(ABC):
             local_vars (dict): A collection of variables provided by the child class instantiation that includes hyperparameter configurations and.
         """
         self._parameters = self._extract_parameters(local_vars)
-        self.output_datatypes = deepcopy(self.default_output_datatypes) # Can be overwritten by child classes
+        self.output_datatype = self.default_output_datatype
         
     @property
     def parameters(self):
@@ -126,17 +123,17 @@ class StepBase(ABC):
         This decorator is designed for preprocessing steps implemented as Python functions,
         where the input to the function is a TensorFlow tensor.
         """
-        def tensor_to_py_function_wrapper(self, image_tensor, target_tensor):
+        def tensor_to_py_function_wrapper(self, image_tensor):
             processed_image = function(self, image_tensor)
-            processed_image = tf.cast(processed_image, dtype=self.output_datatypes['image'])
-            return processed_image, target_tensor
+            processed_image = tf.cast(processed_image, dtype=self.output_datatype)
+            return processed_image
 
         def dataset_map_function(self, image_dataset):
             return image_dataset.map(
-                lambda img, tgt: tf.py_function(
-                    func=lambda i, t: tensor_to_py_function_wrapper(self, i, t),
-                    inp=[img, tgt],
-                    Tout=(self.output_datatypes['image'], self.output_datatypes['target'])
+                lambda img: tf.py_function(
+                    func=lambda i: tensor_to_py_function_wrapper(self, i),
+                    inp=[img],
+                    Tout=(self.output_datatype)
                 )
             )
 
@@ -149,29 +146,29 @@ class StepBase(ABC):
         This decorator is useful for preprocessing steps implemented in Python,
         converting TensorFlow tensors to NumPy arrays before processing.
         """
-        def numpy_to_py_function_wrapper(self, image_tensor, target_tensor):
+        def numpy_to_py_function_wrapper(self, image_tensor):
             image_nparray = image_tensor.numpy().astype('uint8')
             processed_image = function(self, image_nparray)
-            processed_image = tf.convert_to_tensor(processed_image, dtype=self.output_datatypes['image'])
+            processed_image = tf.convert_to_tensor(processed_image, dtype=self.output_datatype)
             processed_image = correct_image_tensor_shape(processed_image)
-            return processed_image, target_tensor
+            return processed_image
 
         def py_function_dataset_map(self, image_dataset):
             return image_dataset.map(
-                lambda img, tgt: tf.py_function(
-                    func=lambda i, t: numpy_to_py_function_wrapper(self, i, t),
-                    inp=[img, tgt],
-                    Tout=(self.output_datatypes['image'], self.output_datatypes['target'])
+                lambda img: tf.py_function(
+                    func=lambda i: numpy_to_py_function_wrapper(self, i),
+                    inp=[img],
+                    Tout=(self.output_datatype)
                 )
             )
 
         return py_function_dataset_map
 
     @abstractmethod    
-    def process_step(self, image_tensor, tf_target):
+    def process_step(self, image_tensor):
         # Child class must implement this method.
-        pass
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
-    print(StepBase.default_output_datatypes)
+    print(StepBase.default_output_datatype)
