dd37157c450d8c8edaca51172382dac1493a9e62
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-03-29 11:23:29 +0100

    Add test runner script for model tests.

---
+++ source/model/tests/test_runner.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from source.model.tests import image_classifiers_trainer_test
+
+from source.utils.test_result_logger import TestResultLogger
+
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..','..')
+OUTPUT_DIR = os.path.join(ROOT_DIR, r'source/model/tests/outputs')
+LOG_FILE = os.path.join(OUTPUT_DIR, 'test_results.log')
+
+
+def load_tests(test_suite):
+    test_suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(image_classifiers_trainer_test))
+    return test_suite
+
+if __name__ == '__main__':
+
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    TestResultLogger(LOG_FILE) # Initialize Test Result Logger.
+
+    test_suite = unittest.TestSuite()
+    unittest.TextTestRunner().run(load_tests(test_suite))
