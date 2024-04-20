ee8ff4d2df66f1f81bae2684e56ba72014301e3e
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-20 17:46:59 +0200

    Add generate_report_elements to generate_report_functions.py.

--- source/model/helpers/generate_report_functions.py
+++ source/model/helpers/generate_report_functions.py
@@ -1,6 +1,6 @@
-
 import os
 import pickle
+from source.model.definitions.general import REPORT_ELEMENT as ELEMENT
 
 
 def get_experiments_data(project_directory, pickle_filename):
@@ -59,59 +59,61 @@ def sort_experiments_data(experiments_data, sort_criteria):
                 msg = f"Metric {metric} not found in experiment {path}."
         raise ValueError(f"{msg}") from e
 
-# def generate_report_elements(project_directory, experiments_data, figure_names, sort_criteria):
-#     """
-#     Generate report elements as an ordered list of tuples.
+def make_experiment_name(relative_path):
+    names = relative_path.split(os.sep)[::-1]
+    title = '_'.join(names[:])
+    return title
 
-#     Args:
-#         project_directory (str): The root directory of the project.
-#         experiments_data (list): A list of tuples containing the relative path of the 
-#             experiment directory and the data loaded from the pickle file.
-#         figure_names (list): The names of the figures to be included in the report for each experiment.
-#         sort_criteria (tuple): The sorting criteria for the experiments data.
+def get_figure_path(project_directory, relative_path, figure_name):
+    dir = os.path.join(project_directory, relative_path)
+    for file in os.listdir(dir):
+        if file.startswith(figure_name):
+            return os.path.join(dir, file)
+    return None
+
+def generate_report_elements(project_directory, experiments_data, header, 
+                                figure_names, summary_metric):
+    """
+    Generate report elements as an ordered list of tuples.
+
+    Args:
+        project_directory (str): The root directory of the project.
+        experiments_data (list): A list of tuples containing the relative path of the 
+            experiment directory and the data loaded from the pickle file.
+        figure_names (list): The names of the figures to be included in the report for each 
+            experiment.
+        no_name (str): The metric to be used as the primary in summary table.
     
-#     Returns:
-#         list: An ordered list of tuples, where the first element is the component type 
-#             ('title', 'table', 'figure', 'text') and the second is the content for that component.
-#     """
-#     document_components = []
+    Returns:
+        list: An ordered list of tuples, where the first element is the component type 
+            ('title', 'table', 'figure', 'text') and the second is the content for that component.
+    """
+    document_components = []
+    document_components.append((ELEMENT.HEADER, header))
+    document_components.append((ELEMENT.TITLE, 'Result Summary'))
+    summary_table = {}
+    for path, data in experiments_data:
+        experiment_name = make_experiment_name(path)
+        summary_table[experiment_name] = {}
+        for statistic in data:
+            summary_table[experiment_name][statistic] = data[statistic][summary_metric]
+    document_components.append((ELEMENT.TABLE, summary_table))
+    document_components.append((ELEMENT.TITLE, 'Experiment Results'))
 
-#     # Title of the report
-#     document_components.append(('title', '# Experiment Results Report\n'))
-
-#     # Summary table setup
-#     headers = ['Statistic'] + [exp_data[0] for exp_data in experiments_data]
-#     table_lines = [headers] + [['---'] * len(headers)]
-
-#     # Data for summary table
-#     metrics = list(experiments_data[0][1][sort_criteria[1]].keys())
-#     for metric in metrics:
-#         row = [f'{metric} ({sort_criteria[1]})'] + [f'{exp_data[1][sort_criteria[1]][metric]:.2f}' for exp_data in experiments_data]
-#         table_lines.append(row)
-
-#     document_components.append(('table', table_lines))
-
-#     # Individual experiment details
-#     for exp_data in experiments_data:
-#         document_components.append(('header', f'## Experiment: {exp_data[0]}\n'))
-
-#         # Figures
-#         for figure in figure_names:
-#             figure_path = os.path.join(project_directory, exp_data[0], figure)
-#             if os.path.exists(figure_path):
-#                 document_components.append(('figure', f'![{figure}]({figure_path})\n'))
-#             else:
-#                 document_components.append(('text', f'Figure {figure} not available.\n'))
-
-#         # Detailed statistics table
-#         headers = ['Statistic'] + list(exp_data[1]['mean'].keys())
-#         table_lines = [headers] + [['---'] * len(headers)]
-#         for statistic in exp_data[1].keys():
-#             row = [statistic] + [f'{metric:.2f}' for metric in exp_data[1][statistic].values()]
-#             table_lines.append(row)
-#         document_components.append(('table', table_lines))
-
-#     return document_components
+    for experiment_data in experiments_data:
+        experiment_name = make_experiment_name(experiment_data[0])
+        document_components.append((ELEMENT.SUBTITLE, experiment_name))
+        for figure in figure_names:
+            figure_path = get_figure_path(project_directory, experiment_data[0], figure)
+            if figure_path:
+                document_components.append((ELEMENT.FIGURE, figure_path))
+            else:
+                red_text = f'Figure {figure} not available in {experiment_name}.'
+                document_components.append((ELEMENT.RED_TEXT, red_text))
+        document_components.append((ELEMENT.TABLE, experiment_data[1]))
+        link = ('link to experiment', experiment_data[0])
+        document_components.append((ELEMENT.LINK, link))
+    return document_components
 
 
 
