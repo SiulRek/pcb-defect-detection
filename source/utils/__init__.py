0d6b4b75db0dc93a97b5530eb779711c9d82d79b
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-20 17:17:12 +0200

    Add Windows-specific import for SimplePopupHandler in utils/__init__.py.

--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -1,9 +1,16 @@
 from source.utils.pcb_visualization import PCBVisualizerforCV2, PCBVisualizerforTF
 from source.utils.recursive_type_conversion import recursive_type_conversion
 from source.utils.class_instances_serializer import ClassInstancesSerializer
-from source.utils.simple_popup_handler import SimplePopupHandler
 from source.utils.parse_and_repeat import parse_and_repeat
 from source.utils.get_sample_from_distribution import get_sample_from_distribution
 from source.utils.logger import Logger
 from source.utils.test_result_logger import TestResultLogger
 from source.utils.copy_json_exclude_entries import copy_json_exclude_entries
+
+# This is a Windows-specific import
+from os import name
+if name == 'nt':
+    from source.utils.simple_popup_handler import SimplePopupHandler
+else:
+    from unittest.mock import MagicMock
+    SimplePopupHandler = MagicMock
