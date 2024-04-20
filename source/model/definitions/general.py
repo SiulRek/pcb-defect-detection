e903c5b236382b59125d0284af3c00068577d346
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-20 17:43:40 +0200

    Add REPORT_ELEMENT enum to general.py.

--- source/model/definitions/general.py
+++ source/model/definitions/general.py
@@ -1,3 +1,16 @@
+from enum import Enum
+
 STRING_SEPARATOR = "-"
 
 RESULT_FILE_NAME = "evaluation_results.pkl"
+
+
+class REPORT_ELEMENT(Enum):
+    HEADER = 'header'
+    TITLE = 'title'
+    SUBTITLE = 'subtitle'
+    TEXT = 'text'
+    RED_TEXT = 'red_text'
+    FIGURE = 'figure'
+    TABLE = 'table'
+    LINK = 'link'
