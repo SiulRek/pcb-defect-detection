8f304894575827aff3d4fc03308db0ef2ae3446e
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-22 15:01:20 +0200

    Refactor folder utils to have subfolders.

--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -1,12 +1,11 @@
-from source.utils.pcb_visualization import PCBVisualizerforCV2, PCBVisualizerforTF
-from source.utils.get_sample_from_distribution import get_sample_from_distribution
-from source.utils.logger import Logger
-from source.utils.test_result_logger import TestResultLogger
+from source.utils.general.pcb_visualization import PCBVisualizerforCV2, PCBVisualizerforTF
+from source.utils.general.get_sample_from_distribution import get_sample_from_distribution
+from source.utils.testing.test_result_logger import TestResultLogger
 
 # This is a Windows-specific import
 from os import name
 if name == 'nt':
-    from source.utils.simple_popup_handler import SimplePopupHandler
+    from source.utils.general.simple_popup_handler import SimplePopupHandler
 else:
     from unittest.mock import MagicMock
     SimplePopupHandler = MagicMock
