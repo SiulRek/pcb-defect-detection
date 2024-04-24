4bddfcaa11845b38e2f480d5a6c248867cf78d9c
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-24 09:34:41 +0200

    Refactor serialize_sample function to support jpeg also.

--- source/data_handling/tfrecord_serialization/serialize.py
+++ source/data_handling/tfrecord_serialization/serialize.py
@@ -8,8 +8,9 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def serialize_sample(image, label):
-    """Serializes an image-label pair into a TFRecord-compatible Example object."""
+def serialize_sample_for_png(image, label):
+    """Serializes an image and label pair to a tf.train.Example proto. The image is expected to be a
+    PNG image."""
     feature = {
         "image": _bytes_feature(tf.io.encode_png(image).numpy()),
         "label": _bytes_feature(tf.io.serialize_tensor(label).numpy()),
@@ -17,9 +18,35 @@ def serialize_sample(image, label):
     sample_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return sample_proto.SerializeToString()
 
+def serialize_sample_for_jpeg(image, label):
+    """Serializes an image and label pair to a tf.train.Example proto. The image is expected to be a
+    JPEG image."""
+    feature = {
+        "image": _bytes_feature(tf.io.encode_jpeg(image).numpy()),
+        "label": _bytes_feature(tf.io.serialize_tensor(label).numpy()),
+    }
+    sample_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return sample_proto.SerializeToString()
 
-def serialize_dataset_to_tf_record(dataset, filepath):
-    """Saves a tf.data.Dataset object to a TFRecord file."""
+
+def serialize_dataset_to_tf_record(dataset, filepath, image_format):
+    """Saves a dataset to a TFRecord file.
+
+    Args:
+        dataset (tf.data.Dataset): A dataset object containing image and label pairs.
+        filepath (str): The path to the output TFRecord file.
+        image_format (str): The format of the images in the dataset. It can be either 'png' or 
+            'jpeg'.
+    """
+    if image_format == 'png':
+        serialize_sample = serialize_sample_for_png
+    elif image_format == 'jpeg':
+        serialize_sample = serialize_sample_for_jpeg
+    else:
+        msg = "Image format '{}' is not supported.".format(image_format)
+        msg += " Please use either 'png' or 'jpeg'."
+        raise ValueError(msg)
+    
     if not os.path.exists(os.path.dirname(filepath)):
         raise FileNotFoundError(
             f"Directory '{os.path.dirname(filepath)}' does not exist."
