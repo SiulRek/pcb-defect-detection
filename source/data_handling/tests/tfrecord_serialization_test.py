b82b8cb7d57585554339ff87f293c29fabc9880a
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-04-24 09:34:53 +0200

    Refactor serialization and deserialization tests for JPEG and PNG datasets.

--- source/data_handling/tests/tfrecord_serialization_test.py
+++ source/data_handling/tests/tfrecord_serialization_test.py
@@ -11,11 +11,14 @@ from source.data_handling.tfrecord_serialization.deserialize import (
 )
 from source.utils import TestResultLogger
 
+
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
 OUTPUT_DIR = os.path.join(ROOT_DIR, "source", "data_handling", "tests", "outputs")
 DATA_DIR = os.path.join(ROOT_DIR, "source", "data_handling", "tests", "data")
+RESTORED_DATA_DIR = os.path.join(OUTPUT_DIR, "restored_data")
 LOG_FILE = os.path.join(OUTPUT_DIR, "test_results.log")
-TFRECORD_FILE = os.path.join(OUTPUT_DIR, "test.tfrecord")
+JPEG_TFRECORD_FILE = os.path.join(OUTPUT_DIR, "jpeg_test.tfrecord")
+PNG_TFRECORD_FILE = os.path.join(OUTPUT_DIR, "png_test.tfrecord")
 
 
 class TestTFRecordHandling(unittest.TestCase):
@@ -31,57 +34,90 @@ class TestTFRecordHandling(unittest.TestCase):
         return image, tf.cast(label, tf.int8)
 
     def setUp(self):
-        # Load images as byte strings and then decode
-        self.data_dicts = [
+        self.jpeg_data_dicts = [
             {"path": "figure_1.jpeg", "category_codes": 0},
             {"path": "figure_2.jpeg", "category_codes": 1},
             {"path": "figure_3.jpeg", "category_codes": 2},
-            # {"path": "figure_4.png", "category_codes": 3},
-            # {"path": "figure_5.png", "category_codes": 4},
         ]
-        for data_dict in self.data_dicts:
+        self.png_data_dicts = [
+            {"path": "figure_4.png", "category_codes": 3},
+            {"path": "figure_5.png", "category_codes": 4},
+        ]
+        for data_dict in self.jpeg_data_dicts + self.png_data_dicts:
             data_dict["path"] = os.path.join(DATA_DIR, data_dict["path"])
 
-        # Create a dataset that loads the images
-        self.dataset = tf.data.Dataset.from_tensor_slices(
+        self.jpeg_dataset = tf.data.Dataset.from_tensor_slices(
             (
-                [data_dict["path"] for data_dict in self.data_dicts],
-                [data_dict["category_codes"] for data_dict in self.data_dicts],
+                [data_dict["path"] for data_dict in self.jpeg_data_dicts],
+                [data_dict["category_codes"] for data_dict in self.jpeg_data_dicts],
+            )
+        ).map(self.load_and_decode_image)
+
+        self.png_dataset = tf.data.Dataset.from_tensor_slices(
+            (
+                [data_dict["path"] for data_dict in self.png_data_dicts],
+                [data_dict["category_codes"] for data_dict in self.png_data_dicts],
             )
         ).map(self.load_and_decode_image)
 
     def tearDown(self):
         self.logger.log_test_outcome(self._outcome.result, self._testMethodName)
-        if os.path.exists(TFRECORD_FILE):
-            os.remove(TFRECORD_FILE)
+        if os.path.exists(JPEG_TFRECORD_FILE):
+            os.remove(JPEG_TFRECORD_FILE)
+        if os.path.exists(PNG_TFRECORD_FILE):
+            os.remove(PNG_TFRECORD_FILE)
 
-    def test_serialize_deserialize(self):
-        """Test the serialization and deserialization of a dataset."""
-        serialize_dataset_to_tf_record(self.dataset, TFRECORD_FILE)
-        restored_dataset = deserialize_dataset_from_tfrecord(TFRECORD_FILE)
+    def test_serialize_deserialize_jpeg(self):
+        """Test the serialization and deserialization of a JPEG dataset."""
+        serialize_dataset_to_tf_record(self.jpeg_dataset, JPEG_TFRECORD_FILE, "jpeg")
+        restored_dataset = deserialize_dataset_from_tfrecord(JPEG_TFRECORD_FILE)
 
-        for original, restored in zip(self.dataset, restored_dataset):
+        for i, (original, restored) in enumerate(zip(self.jpeg_dataset, restored_dataset)):
             original_image, original_label = original
             restored_image, restored_label = restored
 
-            # Check if the images are close enough after serialization and JPEG compression
+            restored_image_path = os.path.join(RESTORED_DATA_DIR, f"restored_jpeg_{i}.jpeg")
+            tf.io.write_file(restored_image_path, tf.io.encode_jpeg(restored_image))
+
             self.assertTrue(
-                np.allclose(original_image.numpy(), restored_image.numpy(), atol=1e-5),
-                "Restored images are not close enough to original images.",
+                np.allclose(original_image.numpy(), restored_image.numpy(), atol=100),
+                "Restored images are not close enough to original images."
             )
-            # Check if labels match exactly
             self.assertEqual(
                 original_label.numpy(),
                 restored_label.numpy(),
-                "Restored labels do not match original labels.",
+                "Restored labels do not match original labels."
+            )
+
+    def test_serialize_deserialize_png(self):
+        """Test the serialization and deserialization of a PNG dataset."""
+        serialize_dataset_to_tf_record(self.png_dataset, PNG_TFRECORD_FILE, "png")
+        restored_dataset = deserialize_dataset_from_tfrecord(PNG_TFRECORD_FILE)
+
+        for i, (original, restored) in enumerate(zip(self.png_dataset, restored_dataset)):
+            original_image, original_label = original
+            restored_image, restored_label = restored
+
+            restored_image_path = os.path.join(RESTORED_DATA_DIR, f"restored_png_{i}.png")
+            tf.io.write_file(restored_image_path, tf.io.encode_png(restored_image))
+
+            self.assertTrue(
+                np.allclose(original_image.numpy(), restored_image.numpy(), atol=1e-5),
+                "Restored images are not close enough to original images."
+            )
+            self.assertEqual(
+                original_label.numpy(),
+                restored_label.numpy(),
+                "Restored labels do not match original labels."
             )
 
     def test_file_not_found_error_on_serialization(self):
         """Test if the correct exception is raised when the directory does not exist for serialization."""
         with self.assertRaises(FileNotFoundError):
             serialize_dataset_to_tf_record(
-                self.dataset,
+                self.jpeg_dataset,
                 os.path.join(ROOT_DIR, "non_existent_directory", "test.tfrecord"),
+                "jpeg",  
             )
 
     def test_file_not_found_error_on_deserialization(self):
