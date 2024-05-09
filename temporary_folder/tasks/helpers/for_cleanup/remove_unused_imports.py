0b12e6acba857dddd7affb21590a63b77ffdd43d
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-09 10:52:27 +0200

    Updated handling of alias names.

--- temporary_folder/tasks/helpers/for_cleanup/remove_unused_imports.py
+++ temporary_folder/tasks/helpers/for_cleanup/remove_unused_imports.py
@@ -1,29 +1,54 @@
 import os
-#TODO Think about how to handle the case where the import statement is split across multiple lines and with indentation
+import re
+
 from temporary_folder.tasks.helpers.for_cleanup.is_specifier_used import is_specifier_used
 from temporary_folder.tasks.helpers.for_cleanup.split_import_statement import split_import_statement
 from temporary_folder.tasks.helpers.for_cleanup.separate_imports import separate_imports
 
 
+def reconstruct_specifier_string(specifier):
+    original_name, alias_name = specifier
+    if original_name == alias_name:
+        return original_name
+    return f"{original_name} as {alias_name}"
+
+
 def remove_unused_imports(code_text):
+    """
+    Removes unused imports from a Python code string.
+
+    Args:
+    code_text (str): The Python code string to remove unused imports from.
+
+    Returns:
+    str: The Python code string with unused imports removed.
+    """
     updated_code = code_text
     import_statements, code_lines = separate_imports(code_text)
     for statement in import_statements:
-        base, specifiers = split_import_statement(statement)
+        base, original_names, alias_names = split_import_statement(statement)
+        specifiers = zip(original_names, alias_names)
         used_specifiers = []
         for specifier in specifiers:
-            if is_specifier_used(specifier, code_lines, include_imports=True):
+            if is_specifier_used(specifier[1], code_lines, include_imports=True):
                 used_specifiers.append(specifier)
         if used_specifiers == []:
-            updated_code = updated_code.replace(statement + '\n', "")
-            updated_code = updated_code.replace(statement, "")
+            pattern = rf"{statement} *\n"
+            updated_code = re.sub(pattern, '', updated_code)
         elif used_specifiers != specifiers:
+            used_specifiers = list(map(reconstruct_specifier_string, used_specifiers))
             restored_import_statement = base + " " + ", ".join(used_specifiers)
             updated_code = updated_code.replace(statement, restored_import_statement)
     return updated_code
 
 
 def remove_unused_imports_from_file(file_path):
+    """
+    Removes unused imports from a Python file.
+
+    Args:
+    file_path (str): The path to the Python file to remove unused imports from.
+    """
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"The file {file_path} does not exist.")
     with open(file_path, 'r') as file:
@@ -32,6 +57,8 @@ def remove_unused_imports_from_file(file_path):
     with open(file_path, 'w') as file:
         file.write(updated_code)
 
+
 if __name__ == "__main__":
     path = r"./temporary_folder/tasks/tests/data/example_script_5.py"
     remove_unused_imports_from_file(path)
+    print(f"Unused imports removed from {path}.")
