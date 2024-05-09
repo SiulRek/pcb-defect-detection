80d70c68b1a701770b4ff00e3004e3969d5de0c1
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-09 15:50:31 +0200

    Extracted duplicated functionalities in line validation.

--- temporary_folder/tasks/helpers/for_create_query/line_validation.py
+++ temporary_folder/tasks/helpers/for_create_query/line_validation.py
@@ -21,6 +21,7 @@ from temporary_folder.tasks.constants.definitions import (
 )
 from temporary_folder.tasks.constants.defaults import DIRECTORY_TREE_DEFAULTS
 
+
 ROUND_BRACKET_PATTERN = re.compile(r"\((.*?)\)")
 SQUARE_BRACKET_PATTERN = re.compile(r"\[(.*?)\]")
 
@@ -36,7 +37,20 @@ def get_list_in_square_brackets(string, error_message):
     return out_list
 
 
-def get_optional_arguments(string): pass #TODO
+def get_optional_arguments(string):
+    """ Get optional arguments from a string."""
+    result = re.search(ROUND_BRACKET_PATTERN, string)
+    if result:
+        arguments = result.group(1).split(",")
+        arguments = [argument.strip() for argument in arguments]
+        return arguments
+    return None
+
+
+def get_bool(string):
+    """ Get a boolean value from a string."""
+    return True if string.strip().lower() == "true" else False
+
 
 def line_validation_for_start_tag(line):
     """ Validate if the line is a start tag."""
@@ -118,20 +132,14 @@ def line_validation_for_directory_tree(line):
         max_depth = DIRECTORY_TREE_DEFAULTS.MAX_DEPTH.value
         include_files = DIRECTORY_TREE_DEFAULTS.INCLUDE_FILES.value
         ignore_list = DIRECTORY_TREE_DEFAULTS.IGNORE_LIST.value
-        result = re.search(ROUND_BRACKET_PATTERN, line)
-        if result:
-            arguments = result.group(1).split(",")
-            arguments = [arg.strip() for arg in arguments]
+        if arguments := get_optional_arguments(line):
             if len(arguments) >= 1:
                 max_depth = int(arguments[0])
             if len(arguments) >= 2:
-                include_files = True if arguments[1].lower() == "true" else False
+                include_files = get_bool(arguments[1])
             if len(arguments) == 3:
-                match = SQUARE_BRACKET_PATTERN.match(arguments[2])
-                if not match:
-                    raise ValueError("Invalid directory tree arguments")
-                additional_ignore_list = match.group(1).split(";")
-                ignore_list.extend([ignore.strip() for ignore in additional_ignore_list])
+                err_msg = "Invalid ignore list in directory tree"
+                ignore_list = get_list_in_square_brackets(arguments[2], err_msg)
         return (dir, max_depth, include_files, ignore_list)
     return None
 
@@ -155,11 +163,8 @@ def line_validation_for_summarize_folder(line):
         err_msg = "Invalid summarize folder arguments"
         excluded_dirs = []
         excluded_files = []
-        if result := re.search(ROUND_BRACKET_PATTERN, line):
-            arguments = result.group(1).split(",")
-            arguments = [argument.strip() for argument in arguments]
-            bool = True if arguments[0].lower() == "true" else False
-            include_definitions_without_docstrings = bool
+        if arguments := get_optional_arguments(line):
+            include_definitions_without_docstrings = get_bool(arguments[0])
             if len(arguments) > 1:
                 excluded_dirs = get_list_in_square_brackets(arguments[1], err_msg)
             if len(arguments) > 2:
@@ -174,10 +179,8 @@ def line_validation_for_make_query(line):
     if MAKE_QUERY_TAG in line:
         max_tokens = None
         create_python_script = False
-        if result := re.search(ROUND_BRACKET_PATTERN, line):
-            arguments = result.group(1).split(",")
-            arguments = [argument.strip() for argument in arguments]
-            create_python_script = True if arguments[0].strip().lower() == "true" else False
+        if arguments := get_optional_arguments(line):
+            create_python_script = get_bool(arguments[0])
             if len(arguments) > 1:
                 max_tokens = int(arguments[1])
         return True, create_python_script, max_tokens
