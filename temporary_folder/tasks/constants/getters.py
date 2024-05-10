028f337c9dda6fda633fcd221cba7e27770931bc
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-10 18:35:43 +0200

    Normalize the paths before returning.

--- temporary_folder/tasks/constants/getters.py
+++ temporary_folder/tasks/constants/getters.py
@@ -1,38 +1,44 @@
 import os
 import json
 
-
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(FILE_DIR, "..", "..", "..")
 
 
 def get_response_file_path(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "temporary_folder", "tasks", "outputs", "answer.txt")
+    path = os.path.join(root_dir, "temporary_folder", "tasks", "outputs", "answer.txt")
+    return os.path.normpath(path)
 
 
 def get_temporary_file_path(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "temporary_folder", "tasks", "outputs", "temporary_file.txt")
+    path = os.path.join(root_dir, "temporary_folder", "tasks", "outputs", "temporary_file.txt")
+    return os.path.normpath(path)
 
 
 def get_fill_text_directory(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "temporary_folder", "tasks", "data", "fill_texts")
+    path = os.path.join(root_dir, "temporary_folder", "tasks", "data", "fill_texts")
+    return os.path.normpath(path)
 
 
 def get_query_templates_directory(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "temporary_folder", "tasks", "data", "query_templates")
+    path = os.path.join(root_dir, "temporary_folder", "tasks", "data", "query_templates")
+    return os.path.normpath(path)
 
 
 def get_output_directory(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "temporary_folder", "tasks", "outputs")
+    path = os.path.join(root_dir, "temporary_folder", "tasks", "outputs")
+    return os.path.normpath(path)
 
 
 def get_checkpoint_directory(root_dir=ROOT_DIR):
     output_dir = get_output_directory(root_dir)
-    return os.path.join(output_dir, "checkpoint")
+    path = os.path.join(output_dir, "checkpoint")
+    return os.path.normpath(path)
 
 
 def get_environment_path(root_dir=ROOT_DIR):
-    return os.path.join(root_dir, "venv")
+    path = os.path.join(root_dir, "venv")
+    return os.path.normpath(path)
 
 
 def get_modules_info(root_dir=ROOT_DIR):
