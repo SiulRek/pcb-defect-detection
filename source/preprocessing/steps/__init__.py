fa809f9581e791e357d0e02d4de116f69ef37bfa
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-02 17:40:18 +0200

    Sorted order of steps alphabetically

--- source/preprocessing/steps/__init__.py
+++ source/preprocessing/steps/__init__.py
@@ -1,50 +1,52 @@
 """ This module contains all preprocessing steps that can be used in an image preprocessing
 pipeline."""
 
+
 from source.preprocessing.steps.adaptive_histogram_equalization import (
     AdaptiveHistogramEqualizer,
 )
+from source.preprocessing.steps.adaptive_thresholding import AdaptiveThresholder
+from source.preprocessing.steps.average_blurring import AverageBlurFilter
+from source.preprocessing.steps.bilateral_filtering import BilateralFilter
+from source.preprocessing.steps.binary_thresholding import BinaryThresholder
+from source.preprocessing.steps.clipping import Clipper
+from source.preprocessing.steps.dilate_erode_sequencing import DilateErodeSequencer
+from source.preprocessing.steps.dilation_filtering import DilationFilter
+from source.preprocessing.steps.dummy_step import DummyStep
+from source.preprocessing.steps.erosion_filtering import ErosionFilter
+from source.preprocessing.steps.gaussian_blurring import GaussianBlurFilter
+from source.preprocessing.steps.gaussian_noise_injection import GaussianNoiseInjector
 from source.preprocessing.steps.global_histogram_equalization import (
     GlobalHistogramEqualizer,
 )
-from source.preprocessing.steps.gaussian_blurring import GaussianBlurFilter
-from source.preprocessing.steps.median_blurring import MedianBlurFilter
-from source.preprocessing.steps.bilateral_filtering import BilateralFilter
-from source.preprocessing.steps.average_blurring import AverageBlurFilter
-from source.preprocessing.steps.nl_mean_denoising import NLMeanDenoiser
-from source.preprocessing.steps.rgb_to_grayscale import RGBToGrayscale
 from source.preprocessing.steps.grayscale_to_rgb import GrayscaleToRGB
-from source.preprocessing.steps.otsu_thresholding import OstuThresholder
-from source.preprocessing.steps.adaptive_thresholding import AdaptiveThresholder
-from source.preprocessing.steps.binary_thresholding import BinaryThresholder
-from source.preprocessing.steps.truncated_thresholding import TruncatedThresholder
-from source.preprocessing.steps.to_zero_thresholding import ZeroThreshold
-from source.preprocessing.steps.erosion_filtering import ErosionFilter
-from source.preprocessing.steps.dilation_filtering import DilationFilter
-from source.preprocessing.steps.dilate_erode_sequencing import DilateErodeSequencer
-from source.preprocessing.steps.square_shape_padding import SquareShapePadder
-from source.preprocessing.steps.shape_resizing import ShapeResizer
-from source.preprocessing.steps.min_max_normalizing import MinMaxNormalizer
-from source.preprocessing.steps.standard_normalizing import StandardNormalizer
-from source.preprocessing.steps.mean_normalizing import MeanNormalizer
 from source.preprocessing.steps.local_contrast_normalizing import (
     LocalContrastNormalizer,
 )
-from source.preprocessing.steps.reverse_scaling import ReverseScaler
-from source.preprocessing.steps.type_casting import TypeCaster
+from source.preprocessing.steps.mean_normalizing import MeanNormalizer
+from source.preprocessing.steps.median_blurring import MedianBlurFilter
+from source.preprocessing.steps.min_max_normalizing import MinMaxNormalizer
 from source.preprocessing.steps.mirroring import Mirrorer
-from source.preprocessing.steps.rotating import Rotator
-from source.preprocessing.steps.dummy_step import DummyStep
-from source.preprocessing.steps.random_rotation import RandomRotator
-from source.preprocessing.steps.random_flipping import RandomFlipper
-from source.preprocessing.steps.gaussian_noise_injection import GaussianNoiseInjector
+from source.preprocessing.steps.nl_mean_denoising import NLMeanDenoiser
+from source.preprocessing.steps.otsu_thresholding import OstuThresholder
 from source.preprocessing.steps.random_color_jitter import RandomColorJitterer
-from source.preprocessing.steps.clipping import Clipper
 from source.preprocessing.steps.random_cropping import RandomCropper
-from source.preprocessing.steps.random_perspective_transformation import (
-    RandomPerspectiveTransformer,
-)
 from source.preprocessing.steps.random_elastic_transformation import (
     RandomElasticTransformer,
 )
+from source.preprocessing.steps.random_flipping import RandomFlipper
+from source.preprocessing.steps.random_perspective_transformation import (
+    RandomPerspectiveTransformer,
+)
+from source.preprocessing.steps.random_rotation import RandomRotator
 from source.preprocessing.steps.random_sharpening import RandomSharpening
+from source.preprocessing.steps.reverse_scaling import ReverseScaler
+from source.preprocessing.steps.rgb_to_grayscale import RGBToGrayscale
+from source.preprocessing.steps.rotating import Rotator
+from source.preprocessing.steps.shape_resizing import ShapeResizer
+from source.preprocessing.steps.square_shape_padding import SquareShapePadder
+from source.preprocessing.steps.standard_normalizing import StandardNormalizer
+from source.preprocessing.steps.to_zero_thresholding import ZeroThreshold
+from source.preprocessing.steps.truncated_thresholding import TruncatedThresholder
+from source.preprocessing.steps.type_casting import TypeCaster
+
