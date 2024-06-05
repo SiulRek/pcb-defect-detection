3612a47c234523bd71b483367137f73afb8a662d
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-05 10:54:28 +0200

    Added BaseTestCase class and demo.

---
+++ source/testing/base_test_case.py
@@ -0,0 +1,84 @@
+from abc import ABC
+import os
+import unittest
+
+from source.testing.test_result_logger import TestResultLogger
+
+
+class BaseTestCase(unittest.TestCase, ABC):
+    """
+    Abstract base class for all test cases, providing setup and teardown
+    operations that are common across various tests. This class ensures that all
+    derived test classes are prepared with necessary directories and logging
+    capabilities.
+
+    Attributes:
+        - output_dir (str): Directory where test outputs will be saved.
+        - temp_dir (str): Temporary directory for use during tests.
+        - log_file (str): Path to the log file used to record test results.
+        - logger (TestResultLogger): Logger instance to log test outcomes.
+    """
+
+    @classmethod
+    def compute_output_dir(cls, parent_folder="tests"):
+        """
+        Computes the directory path for test outputs by traversing up the file
+        hierarchy until a directory named 'tests' is found. It then returns the
+        path to the 'outputs' subdirectory within 'tests'.
+
+        Args:
+            - parent_folder (str, optional): The name of the parent folder
+                containing the 'tests' directory.
+
+        Returns:
+            - str: The path to the output directory.
+        """
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        while parent_folder not in os.listdir(current_dir):
+            current_dir = os.path.dirname(current_dir)
+            if os.path.splitdrive(current_dir)[1] == os.sep:
+                msg = "Tests directory not found in the path hierarchy."
+                raise NotADirectoryError(msg)
+
+        return os.path.join(current_dir, parent_folder, "outputs")
+
+    @classmethod
+    def get_test_case_name(cls):
+        """
+        Generates a more readable test case name by removing 'Test' prefix and
+        adding spaces before each capital letter in the class name.
+
+        Returns:
+            - str: The formatted test case name.
+        """
+        name = cls.__name__
+        if name.startswith("Test"):
+            name = name[4:]
+        name = [letter if letter.islower() else f" {letter}" for letter in name]
+        return "".join(name)
+
+    @classmethod
+    def setUpClass(cls):
+        """ Class-level setup method that ensures necessary directories are created
+        and initializes logging for the test case. """
+        cls.output_dir = cls.compute_output_dir()
+        if not os.path.exists(cls.output_dir):
+            os.makedirs(cls.output_dir)
+        cls.temp_dir = os.path.join(cls.output_dir, "temp")
+        if not os.path.exists(cls.temp_dir):
+            os.makedirs(cls.temp_dir)
+
+        cls.log_file = os.path.join(cls.output_dir, "test_results.log")
+        cls.logger = TestResultLogger(cls.log_file, cls.get_test_case_name())
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Class-level teardown method that removes the temporary directory created
+        during the test setup. """
+        if os.path.exists(cls.temp_dir):
+            os.rmdir(cls.temp_dir)
+
+    def tearDown(self):
+        """ Instance-level teardown method that logs the outcome of each test
+        method. """
+        self.logger.log_test_outcome(self._outcome.result, self._testMethodName)
---
+++ source/testing/base_test_case_demo.py
@@ -0,0 +1,45 @@
+import unittest
+from source.testing.base_test_case import BaseTestCase
+
+
+class BaseTestCaseDemo(BaseTestCase):
+    """
+    A demonstration class to prove and showcase the functionality of the
+    BaseTestCase. It leverages the setup and teardown mechanisms of BaseTestCase
+    to demonstrate their effectiveness and usage in a practical testing
+    scenario.
+    """
+
+    @classmethod
+    def compute_output_dir(cls):
+        # Overriding the method to avoid the need for a 'tests' directory.
+        return super().compute_output_dir("testing")
+    
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        print(f"SetupClass: Output directory has been set up at {cls.output_dir}")
+        print(f"SetupClass: Temporary directory has been set up at {cls.temp_dir}")
+        print(f"SetupClass: Log file has been set up at {cls.log_file}")
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        print(
+            f"TearDownClass: Temporary directory at {cls.temp_dir} has been cleaned up."
+        )
+
+    def test_example_functionality(self):
+        """ An example test that logs its outcome and demonstrates the logging
+        functionality. """
+        self.assertTrue(True)  
+
+
+    def tearDown(self):
+        super().tearDown()
+        print("Logging the outcome of the test method.")
+
+
+if __name__ == "__main__":
+
+    unittest.main()
