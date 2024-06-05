97e7ea10830bc2f16f847b7bfd2036d3aa3866f3
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-05 15:18:14 +0200

    Removed TestResultsLogger import statement.

--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -6,7 +6,6 @@ from source.utils.general.get_sample_from_distribution import (
     get_sample_from_distribution,
 )
 from source.utils.general.search_files_with_name import search_files_with_name
-from source.utils.testing.test_result_logger import TestResultLogger
 
 # This is a Windows-specific import
 from os import name
